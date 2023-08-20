80f08135621212db9b4901062c039a91445f9115
ThisIzATriumph <80205784+ThisIzATriumph@users.noreply.github.com>
2023-08-20 22:52:01 +0200

    First upload

---
+++ site.config.ts
@@ -0,0 +1,72 @@
+import { siteConfig } from './lib/site-config'
+
+export default siteConfig({
+  // the site's root Notion page (required)
+  rootNotionPageId: 'd64869eabdea42f095dd3e8f1c1cd662',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Circular Odyssey',
+  domain: 'circular-odyssey.com',
+  author: 'Baptiste Sene-Donnais',
+
+  // open graph metadata (optional)
+  description: 'Baptiste Sene explores uncharted areas of the Circular design practice.',
+
+  // social usernames (optional)
+  twitter: '_basendo',
+  github: 'thisizatriumph',
+  linkedin: 'senebaptiste',
+  // newsletter: '#', // optional newsletter URL
+  // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: true,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null,
+
+  // whether to use the default notion navigation style or a custom one with links to
+  // important pages
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    // {
+    //   title: 'Garden',
+    //   pageId: '87efffe7268241fba47dac05ad46dcb0'
+    // },
+    // {
+    //   title: 'Viewpoints',
+    //   pageId: '946949a287f84c8d8aacedb4577a8951'
+    // },
+    {
+      title: 'About',
+      pageId: '157f65d417ec4ed6a341fcade5178820'
+    },
+    // {
+    //   title: 'Contact',
+    //   pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+    // }
+  ]
+})
